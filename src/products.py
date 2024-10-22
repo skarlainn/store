29d1b00c6a2bf87a1b1e2f78f70b807f756325d7
Diana <ddmitrieva098@exaple.com>
2024-10-22 14:45:10 +1100

    add products

---
+++ src/products.py
@@ -0,0 +1,29 @@
+class Product:
+    """Класс с  свойствами: название, описание, цена, количество в наличии"""
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+class Category:
+    """Класс с свойствами: имя, описание, список товаров категории"""
+    name: str
+    description: str
+    products: list
+    product_count = 0
+    category_count = 0
+
+    def __init__(self, name, description, products):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.product_count += len(products)
+        Category.category_count += 1
