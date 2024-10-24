df0c5d5ca115ea7d71f130389bd9dc9dea8a6fde
Diana <ddmitrieva098@exaple.com>
2024-10-24 18:02:42 +1100

    adding new features to products

--- src/products.py
+++ src/products.py
@@ -1,21 +1,6 @@
-class Product:
-    """Класс с  свойствами: название, описание, цена, количество в наличии"""
-
-    name: str
-    description: str
-    price: float
-    quantity: int
-
-    def __init__(self, name, description, price, quantity):
-        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра"""
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
 
 class Category:
-    """Класс с свойствами: имя, описание, список товаров категории"""
+    """Класс со свойствами: имя, описание, список товаров категории"""
 
     name: str
     description: str
@@ -27,6 +12,62 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products else []
         Category.product_count += len(products)
         Category.category_count += 1
+
+
+    def add_product(self, product):
+        """Метод добавляет продукт к списку продуктов в категории"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        """Метод возвращает строку с названием продукта его стоимость и остаток"""
+        products_string = ""
+        for product in self.__products:
+            products_string += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_string
+
+
+class Product:
+    """Класс со свойствами: название, описание, цена, количество в наличии"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра"""
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, product: dict):
+        """Метод добавляет новый продукт"""
+        return cls(**product)
+
+
+    @property
+    def price(self):
+        """Метод возвращает цену продукта"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """Метод позволяет изменить цену продукта"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif new_price < self.__price:
+            confirmation = input(f"Подтвердите понижение цены с {self.__price} до {new_price}: y(да)/n(нет)\n").lower()
+            if confirmation == "y":
+                self.__price = new_price
+        else:
+            self.__price = new_price
+
