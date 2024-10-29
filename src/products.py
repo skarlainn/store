0351ffd157998afdd6dbb01f2ebe582eb4ed2400
Diana <ddmitrieva098@exaple.com>
2024-10-29 18:12:54 +1100

    fixes

--- src/products.py
+++ src/products.py
@@ -1,53 +1,3 @@
-class Category:
-    """Класс со свойствами: имя, описание, список товаров категории"""
-
-    name: str
-    description: str
-    products: list
-    product_count = 0
-    category_count = 0
-
-    def __init__(self, name, description, products):
-        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра"""
-        self.name = name
-        self.description = description
-        self.__products = products if products else []
-        Category.product_count += len(products)
-        Category.category_count += 1
-        self.products_index = 0
-
-    def __str__(self):
-        """Магический метод возвращающий строковое отображение информации о количестве продуктов"""
-        count_products = 0
-        for product in self.__products:
-            count_products += product.quantity
-        return f"{self.name}, количество продуктов: {count_products} шт."
-
-    def __iter__(self):
-        self.products_index = 0
-        return self
-
-    def __next__(self):
-        if self.products_index < len(self.__products):
-            result = self.__products[self.products_index]
-            self.products_index += 1
-            return str(result)
-        else:
-            raise StopIteration
-
-    def add_product(self, product):
-        """Метод добавляет продукт к списку продуктов в категории"""
-        self.__products.append(product)
-        Category.product_count += 1
-
-    @property
-    def products(self):
-        """Метод возвращает строку с названием продукта его стоимость и остаток"""
-        products_string = ""
-        for product in self.__products:
-            products_string += f"{str(product)}\n"
-        return products_string
-
 
 class Product:
     """Класс со свойствами: название, описание, цена, количество в наличии"""
@@ -70,7 +20,10 @@ class Product:
 
     def __add__(self, other):
         """Магический метод возвращает сумму цен двух товаров"""
-        return (self.price * self.quantity) + (other.price * other.quantity)
+        if type(other) is self.__class__:
+            return (self.price * self.quantity) + (other.price * other.quantity)
+        else:
+            raise TypeError
 
     @classmethod
     def new_product(cls, dict_product: dict, products=None):
